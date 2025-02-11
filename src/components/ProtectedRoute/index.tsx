7bb028c50d2fde1afc0568472f73f6fbc5fa0e1d
Jhean Bryan <93744970+jheanbryan@users.noreply.github.com>
2025-02-11 00:06:07 -0400

    removed bug in JSX import

--- src/components/ProtectedRoute/index.tsx
+++ src/components/ProtectedRoute/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { JSX, useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../../Context/UserContext";
 
